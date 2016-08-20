e9a6e7aa34556fda642d6d876b5e1068f8da2c78
Chamikara Dharmasena <chamikaradharmasena@Chamikaras-MacBook-Air.local>
2016-08-20 14:18:14 -0500

    search added

---
+++ pokedex3-swift/PokemonDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex3-swift
+//
+//  Created by Chamikara Dharmasena on 8/20/16.
+//  Copyright © 2016 Chamikara Dharmasena. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
